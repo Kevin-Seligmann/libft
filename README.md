9abc6412c284d1f49f981510006e2ba2b766b983
kevin-seligmann <seligmann.kevin.cc@gmail.com>
2024-09-04 04:26:34 +0200

    Removed subject pdf, added documentation

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is a C library containing utilities. Includes four modules. Module I is an 
 
 #### Module I
 
-ft_isalpha. ft_isdigit, ft_isalnum, ft_isascii, ft_isprint, ft_strlen, ft_memset, ft_bzero, ft_memcpy, ft_memmove, ft_strlcpy, ft_strlcat, ft_toupper, ft_tolower, ft_strchr, ft_strrchr, ft_strncmp, ft_memchr, ft_memcmp, ft_strnstr, ft_atoi, ft_calloc, ft_strdup,
+ft_isalpha. ft_isdigit, ft_isalnum, ft_isascii, ft_isprint, ft_strlen, ft_memset, ft_bzero, ft_memcpy, ft_memmove, ft_strlcpy, ft_strlcat, ft_toupper, ft_tolower, ft_strchr, ft_strrchr, ft_strncmp, ft_memchr, ft_memcmp, ft_strnstr, ft_atoi, ft_calloc, ft_strdup
 
 More information [Apple developer webpage](developer.apple.com)
 
@@ -12,36 +12,23 @@ More information [Apple developer webpage](developer.apple.com)
 
 ft_substr, ft_strjoin, ft_strtrim, ft_split, ft_itoa, ft_strmapi, ft_striteri, ft_putchar_fd, ft_putstr_fd, ft_putendl_fd, ft_putnbr_fd
 
-More information [libft.pdf, part 2](https://github.com/Kevin-Seligmann/libft/blob/main/libft.pdf)
-
 #### Module III
 
 ft_lstnew, ft_lstadd_front, ft_lstsize, ft_lstlast, ft_lstadd_back, ft_lstdelone, ft_lstclear, ft_lstiter, ft_lstmap
-
-More information [libft.pdf, part 3](https://github.com/Kevin-Seligmann/libft/blob/main/libft.pdf)
   
 #### Module IV
-- [get_next_line](https://github.com/Kevin-Seligmann/get_next_line)
-- [ft_printf](https://github.com/Kevin-Seligmann/ft_printf)
-- char	*ft_strsnjoin(char **arr, char *sep, size_t n). Returns an allocated string with the content of arr (Of size n) separated by sep.
-- char	**ft_split_charset(char const *s, char *c). Splits the string s by the characters on c (In the same way as ft_split).
-- void	*ft_lastloc(size_t count, size_t size). Allocates count * size bytes and null terminates it.
-- char	*ft_itoa_base(size_t n, char *base). Transforms a number to the provided base.
-- int	*ft_intnchr(int *arr, int c, int n). Search for the first occurence of c in arr, searchs up to n numbers.
-- void	ft_arr_free_int(int **arr). Frees an array of pointers to int that is null terminated.
-- void	ft_arr_free(char **arr). Frees an array of strings that is null terminated.
-   
+
+[get_next_line](https://github.com/Kevin-Seligmann/get_next_line), [ft_printf](https://github.com/Kevin-Seligmann/ft_printf), ft_strsnjoin, ft_split_charset, ft_lastloc, ft_itoa_base, ft_intnchr, ft_arr_free_int, ft_arr_free
+
+More documentation can be found on the libft.h header file.
+
 The "project" folder contains the files at the time of evaluation. The "libft" folder is an updated version.  
 
 Styleguide: [Norminette](https://github.com/42School/norminette). 
 
 # Usage
 
-- Compile your .o files with flags `-I./libft/inc` (or `-I$(libft_folder)/inc`)
-- Link your program with `./libft_folder/libft.a`
-- Add the include clause where the library will be used. `#include "libft.h"`
-- If a makefile is used, add a rule containing `make --no-print-directory -C $(LFT_FOLDER)` to compile libft.a
-- If a makefile is used, consider adding a rule containing `@$(MAKE) --no-print-directory -C $(LFT_FOLDER) fclean` to remove libft binaries
+Using `make` on this folder will compile a 'libft.a' binary. The 'libft.h' header must be included.
 
 ## 42
 
--- libft/inc/libft.h
+++ libft/inc/libft.h
@@ -6,7 +6,7 @@
 /*   By: kseligma <kseligma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 22:35:32 by kseligma          #+#    #+#             */
-/*   Updated: 2024/07/28 12:11:32 by kseligma         ###   ########.fr       */
+/*   Updated: 2024/09/04 04:26:00 by kseligma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include <limits.h>
-# include "ft_printf.h"
-# include "get_next_line.h"
+
+// Module 1
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -37,33 +37,74 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-int		*ft_intnchr(int *arr, int c, int n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
-void	*ft_lastloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
+
+// Module 2
+
+/*
+	Returns a malloc'd substring of `s` of maximum size `len`, from the `start` position.
+	If the returning string has length 0, it returns an empty string.
+*/
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+/*
+	Returns a string result of joining 's1' and 's2' .
+*/
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strsnjoin(char **p_s, char *sep, size_t n);
+
+/*
+	Returns a string result of removing the characters in `set` from the start and
+	end of `s1`.
+*/
 char	*ft_strtrim(char const *s1, char const *set);
+
+/*
+	Returns a null-terminated array of strings, result of splitting the `s` string by the `c` char.
+*/
 char	**ft_split(char const *s, char c);
-char	**ft_split_charset(char const *s, char *c);
-int		ft_arr_count_arguments(char **arr);
-void	ft_arr_free(char **arr);
-void	ft_arr_free_int(int **arr);
+
+/*
+	Returns a string equivalent to `n`.
+*/
 char	*ft_itoa(int n);
-char	*ft_itoa_base(size_t n, char *base);
+
+/*
+	Returns a string result of applying `f` to all its elements.
+*/
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+/*
+	Applies `f` to a string.
+*/
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+
+/*
+	Writes `c` on the `fd` file.
+*/
 void	ft_putchar_fd(char c, int fd);
+
+/*
+	Writes `s` on the `fd` file.
+*/
 void	ft_putstr_fd(char *s, int fd);
+
+/*
+	Writes `s` on the `fd` file, and a newline.
+*/
 void	ft_putendl_fd(char *s, int fd);
+
+/*
+	Writes the `n` integer on the `fd` file.
+*/
 void	ft_putnbr_fd(int n, int fd);
-int		ft_strcmp(char *s1, char *s2);
+
+// Module 3
 
 typedef struct s_list
 {
@@ -71,14 +112,100 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
+/*
+	Returns a new list node with `content` content.
+*/
 t_list	*ft_lstnew(void *content);
+
+/*
+	Adds the node `new` to the front of the list that starts in `*lst`.
+*/
 void	ft_lstadd_front(t_list **lst, t_list *new);
+
+/*
+	Returns the size of the list `lst`.
+*/
 int		ft_lstsize(t_list *lst);
+
+/*
+	Returns the last node of the list `lst`
+*/
 t_list	*ft_lstlast(t_list *lst);
+
+/*
+	Adds the node `new` to the back of the list starting in `*lst`
+*/
 void	ft_lstadd_back(t_list **lst, t_list *new);
+
+/*
+	Applies the `del` function on the content of `lst` and frees `lst`.
+*/
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+
+/*
+	Applies the `del` function on the content of `lst` and all subsecuent nodes, and frees them.
+*/
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+
+/*
+	Applies `f` to all the members of the `lst` list.
+*/
 void	ft_lstiter(t_list *lst, void (*f)(void *));
+
+/*
+	Returns a list that is the result of applying `f` to all members of `lst`.
+	In case of memory failure, calls `ft_lstclear` on the unfinished new list.
+*/
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+// Module 4
+
+# include "ft_printf.h"
+# include "get_next_line.h"
+
+
+/*
+	Returns a pointer to the first ocurrence of `c` in `arr`. Searchs up to `n` integers.
+*/
+int		*ft_intnchr(int *arr, int c, int n);
+
+/*
+	Allocates `count` * `size` bytes and returns it as void *. Sets the last byte at 0.
+*/
+void	*ft_lastloc(size_t count, size_t size);
+
+/*
+	Returns a string result of joining all elements on `p_s`, appending `sep` on the middle of them.
+	`p_s` must be size `n`.
+*/
+char	*ft_strsnjoin(char **p_s, char *sep, size_t n);
+
+/*
+	Like `ft_split` but with many separators.
+*/
+char	**ft_split_charset(char const *s, char *c);
+
+/*
+	Returns the size of a null-terminated char **.
+*/
+int		ft_arr_count_arguments(char **arr);
+
+/*
+	Frees a null-terminated char **, and all its char *s.
+*/
+void	ft_arr_free(char **arr);
+
+/*
+	Line `ft_arr_free` but with ints.
+*/
+void	ft_arr_free_int(int **arr);
+
+/*
+	Returns a string result of transforming `n` to the base `base`.
+*/
+char	*ft_itoa_base(size_t n, char *base);
+int		ft_strcmp(char *s1, char *s2);
+
+
 
 #endif
